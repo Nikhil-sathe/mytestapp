8b889c9c7f254101c844804dba1b89e38ab00060
Nikhil Sathe <sathenikhil110@gmail.com>
2020-12-10 19:38:51 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld{
+    public static void main(String []args)
+    {
+        System.out.println("Hello World from Jenkins");
+         System.out.println("added another line");
+         int num=100;
+    }
+} 
